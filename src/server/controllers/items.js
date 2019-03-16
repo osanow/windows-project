412325b2ef6c0a54cb6bd86681eb5890b47f306c
osanow <osanow@wp.pl>
2019-03-16 13:22:36 +0100

    Added CRUD operations for user items

--- src/server/controllers/items.js
+++ src/server/controllers/items.js
@@ -1,12 +1,65 @@
 const Item = require('../models/item');
 
-exports.postGetItemsByPath = (req, res) => {
+exports.getItems = (req, res) => {
   const { path } = req.query;
-  Item.find({ path, owner: req.userId })
+
+  const query = { path, owner: req.userId };
+  if (!path) delete query.path;
+
+  Item.find(query)
     .then((items) => {
       res.status(200).json(items);
     })
-    .catch((err) => {
-      console.log(err);
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json({ error });
+    });
+};
+
+exports.postItems = (req, res) => {
+  const { items } = req.body;
+
+  const updatedItems = items.map((item) => {
+    const updatedItem = {
+      ...item,
+      owner: req.userId
+    };
+    return updatedItem;
+  });
+
+  Item.insertMany(updatedItems)
+    .then(() => {
+      res.status(200).json({ message: 'Added new items' });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json({ error });
+    });
+};
+
+exports.putItem = (req, res) => {
+  const { item } = req.body;
+  const { id } = req.params;
+
+  Item.findByIdAndUpdate(id, item)
+    .then(() => {
+      res.status(200).json({ message: 'Updated item' });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json({ error });
+    });
+};
+
+exports.deleteItem = (req, res) => {
+  const { id } = req.params;
+
+  Item.findByIdAndDelete(id)
+    .then(() => {
+      res.status(200).json({ message: 'Deleted item' });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json({ error });
     });
 };
--- src/server/routes/items.js
+++ src/server/routes/items.js
@@ -4,7 +4,9 @@ const itemsController = require('../controllers/items');
 
 const router = express.Router();
 
-// router.get('', itemsController.getItems);
-router.post('/', itemsController.postGetItemsByPath);
+router.post('/', itemsController.postItems);
+router.get('/', itemsController.getItems);
+router.put('/:id', itemsController.putItem);
+router.delete('/:id', itemsController.deleteItem);
 
 module.exports = router;
