f8c31cbd608898f7d4210b700770669c4df7f4d4
osanow <osanow@wp.pl>
2019-03-22 13:03:15 +0100

    Refactor DesktopItem component

--- src/client/containers/Desktop/DesktopIcon/DesktopIcon.js
+++ src/client/containers/Desktop/DesktopIcon/DesktopIcon.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 
 import axios from '../../../axios-instance';
-import { updateObject } from '../../../utils/utility';
+import { updateObject, onCatchHandler } from '../../../utils/utility';
 import { openApp } from '../../../store/actions/index';
 import noIcon from '../../../assets/icons/noIcon.png';
 import * as Styles from './styles';
-
-const calculatePos = (type, value) => {
-  if (type === 'row') return Math.abs(Math.ceil(value / (4.8 * 16)));
-  return Math.abs(Math.ceil(value / (6.3 * 16)));
-};
 
 class Item extends Component {
   state = {
@@ -41,84 +35,6 @@ class Item extends Component {
       })))
       .catch(err => console.log(err));
   }
-
-  onMoveHandler = (e) => {
-    const { isDragging } = this.state;
-    if (!isDragging) return;
-    this.draggingTime = this.draggingTime + 1;
-    const newX = e.clientX - this.prevX;
-    const newY = e.clientY - this.prevY;
-    this.setState(prevState => updateObject(prevState, { position: { x: newX, y: newY } }));
-  };
-
-  onDropHandler = (e) => {
-    const { _id } = this.props;
-
-    this.draggingTime = 0;
-    document.removeEventListener('mouseup', this.onDropHandler, false);
-    document.removeEventListener('mousemove', this.throttledMouseMove, false);
-
-    const posX = e.clientX
-      - (e.target.offsetLeft > 94 || e.target.tagName !== 'DIV'
-        ? 0
-        : e.target.offsetLeft)
-      + (e.target.offsetWidth > 94 || e.target.tagName !== 'DIV'
-        ? 0
-        : e.target.offsetWidth)
-        / 2
-      - 22;
-    const posY = e.clientY
-      - (e.target.offsetTop > 70 || e.target.tagName !== 'DIV'
-        ? 0
-        : e.target.offsetTop)
-      + (e.target.offsetHeight > 70 || e.target.tagName !== 'DIV'
-        ? 0
-        : e.target.offsetHeight)
-        / 2
-      - 16;
-
-    const maxRow = calculatePos('row', window.innerHeight - 128);
-    const currRow = calculatePos('row', posY);
-
-    const maxCol = calculatePos('col', window.innerWidth - 64);
-    const currCol = calculatePos('col', posX);
-
-    const newRow = currRow > maxRow ? maxRow.toString() : currRow.toString();
-    const newCol = currCol > maxCol ? maxCol.toString() : currCol.toString();
-
-    this.prevX = 0;
-    this.prevY = 0;
-
-    this.setState(prevState => updateObject(prevState, {
-      isDragging: false,
-      position: { x: 0, y: 0 },
-      gridPosition: { rowPos: newRow, colPos: newCol }
-    }));
-
-    axios(`/items/${_id}`, {
-      method: 'PUT',
-      data: {
-        changedValues: {
-          rowPos: newRow,
-          colPos: newCol
-        }
-      }
-    }).catch((error) => {
-      console.log(error);
-    });
-  };
-
-  onCatchHandler = (e) => {
-    this.prevX = e.clientX;
-    this.prevY = e.clientY;
-
-    this.throttledMouseMove = _.throttle(this.onMoveHandler, 50);
-    this.setState({ isDragging: true }, () => {
-      this.draggingTime = 0;
-      document.addEventListener('mouseup', this.onDropHandler, false);
-      document.addEventListener('mousemove', this.throttledMouseMove, false);
-    });
-  };
 
   onChangeNameHandler = (e) => {
     const newValue = e.target.value.replace(/[^\w\s]/g, '');
@@ -177,7 +93,7 @@ class Item extends Component {
         loading={appLoading}
         left={position.x}
         top={position.y}
-        onMouseDown={this.onCatchHandler}
+        onMouseDown={e => onCatchHandler(this, e)}
         onDoubleClick={this.onOpenHandler}
         rowPos={gridPosition.rowPos}
         colPos={gridPosition.colPos}
--- src/client/utils/utility.js
+++ src/client/utils/utility.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-param-reassign */
+import _ from 'lodash';
+import axios from '../axios-instance';
+
 export const updateObject = (oldObject, updatedProperties) => ({
   ...oldObject,
   ...updatedProperties
@@ -36,3 +40,89 @@ export const checkValidity = (value, rules) => {
 
 export const changeFormatWithoutDashes = text => text.replace('_', ' ');
 export const changeFormatWithDashes = text => text.replace(' ', '_');
+
+const calculatePos = (type, value) => {
+  if (type === 'row') return Math.abs(Math.ceil(value / (4.8 * 16)));
+  return Math.abs(Math.ceil(value / (6.3 * 16)));
+};
+
+export const onMoveHandler = (icon, e) => {
+  icon.draggingTime += 1;
+  const newX = e.clientX - icon.prevX;
+  const newY = e.clientY - icon.prevY;
+  icon.setState(prevState => updateObject(prevState, { position: { x: newX, y: newY } }));
+};
+
+export const onDropHandler = (icon, e) => {
+  const { _id } = icon.props;
+
+  icon.draggingTime = 0;
+  document.removeEventListener('mouseup', icon.onDropFunction, false);
+  document.removeEventListener('mousemove', icon.throttledMouseMove, false);
+
+  icon.prevX = 0;
+  icon.prevY = 0;
+
+  if (icon.state && icon.state.gridPosition) {
+    const posX = e.clientX
+      - (e.target.offsetLeft > 94 || e.target.tagName !== 'DIV'
+        ? 0
+        : e.target.offsetLeft)
+      + (e.target.offsetWidth > 94 || e.target.tagName !== 'DIV'
+        ? 0
+        : e.target.offsetWidth)
+        / 2
+      - 22;
+    const posY = e.clientY
+      - (e.target.offsetTop > 70 || e.target.tagName !== 'DIV'
+        ? 0
+        : e.target.offsetTop)
+      + (e.target.offsetHeight > 70 || e.target.tagName !== 'DIV'
+        ? 0
+        : e.target.offsetHeight)
+        / 2
+      - 16;
+
+    const maxRow = calculatePos('row', window.innerHeight - 128);
+    const currRow = calculatePos('row', posY);
+
+    const maxCol = calculatePos('col', window.innerWidth - 64);
+    const currCol = calculatePos('col', posX);
+
+    const newRow = currRow > maxRow ? maxRow.toString() : currRow.toString();
+    const newCol = currCol > maxCol ? maxCol.toString() : currCol.toString();
+
+
+    icon.setState(prevState => updateObject(prevState, {
+      isDragging: false,
+      position: { x: 0, y: 0 },
+      gridPosition: { rowPos: newRow, colPos: newCol }
+    }));
+
+    axios(`/items/${_id}`, {
+      method: 'PUT',
+      data: {
+        changedValues: {
+          rowPos: newRow,
+          colPos: newCol
+        }
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+};
+
+export const onCatchHandler = (icon, e) => {
+  console.log(icon, e);
+  icon.prevX = e.clientX;
+  icon.prevY = e.clientY;
+
+  icon.throttledMouseMove = _.throttle(ev => onMoveHandler(icon, ev), 50);
+  icon.onDropFunction = ev => onDropHandler(icon, ev);
+  icon.setState({ isDragging: true }, () => {
+    icon.draggingTime = 0;
+    document.addEventListener('mouseup', icon.onDropFunction, false);
+    document.addEventListener('mousemove', icon.throttledMouseMove, false);
+  });
+};
