de7794131ef0aa5e4afe8745122deee407596817
osanow <osanow@wp.pl>
2019-04-29 20:57:13 +0200

    Deployment changes

--- src/client/axios-instance.js
+++ src/client/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/' // 'https://windows-project.herokuapp.com/'
+  baseURL: 'https://windows-project.herokuapp.com/' // 'http://localhost:8080/'
 });
 
 export default instance;
